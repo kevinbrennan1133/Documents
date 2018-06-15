8240ca01778a951ed048ca46ba94005e1448227b
Kevin Brennan <kevinbrennan@mwc-136182.dhcp.missouri.edu>
2018-06-15 13:18:32 -0500

    Storyboard works but I have not found a way to save the text

--- Documents/Documents/DocumentViewController.swift
+++ Documents/Documents/DocumentViewController.swift
@@ -10,19 +10,36 @@ import UIKit
 
 class DocumentViewController: UIViewController {
 
+    
+    
+  //  let documents = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+
+/*
+    let documents = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+    let writePath = documents.stringByAppendingPathComponent("file.plist")
+    
+    let array = NSArray(contentsOfFile: filePath)
+    if let array = array {
+        array.writeToFile(filePath, atomically: true)
+    }
+
+*/
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
 
-    /*
+
+   
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -30,6 +47,6 @@ class DocumentViewController: UIViewController {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
+
 
 }
--- Documents/Documents/TextViewController.swift
+++ Documents/Documents/TextViewController.swift
@@ -9,23 +9,46 @@
 import UIKit
 
 class TextViewController: UIViewController {
-
-    @IBOutlet weak var Titlelabel: UINavigationItem!
-    
+    @IBOutlet weak var textbox: UITextView!
+    @IBOutlet weak var titleName: UINavigationItem!
     @IBOutlet weak var textName: UITextField!
     
-    
+   /* var files : FileManager = FileManager.default
+    var docsDir: String?
+    var dataFile: String = ""
+
+    */
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = textName?.text
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+   /* @IBAction func Save(_ sender: Any) {
+        if let text = textbox?.text {
+            let dataBuffer = text.data(using: String.Encoding.utf8)
+            
+            files.createFile(atPath: dataFile, contents: dataBuffer, attributes: nil)
+        }
+    }
     
+    func checkFile() {
+        
+        let dirPaths = files.urls(for: .documentDirectory, in: .userDomainMask)
+        dataFile = dirPaths[0].appendingPathComponent("datafile.dat").path
+        if files.fileExists(atPath: dataFile) {
+            if let databuffer = files.contents(atPath: dataFile) {
+                let datastring = NSString(data: databuffer, encoding:String.Encoding.utf8.rawValue)
+                textbox.text = datastring as String?
+            }
+        }
+    }
+*/
 
     /*
     // MARK: - Navigation
